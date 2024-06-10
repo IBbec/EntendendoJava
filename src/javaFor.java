13c24e6182db7ef9f68a6cc62fb9e05a31a62ea2
Rebeca <rebeca.pinheiro2601@gmail.com>
2024-06-10 17:01:00 -0300

    arrays

--- src/javaFor.java
+++ src/javaFor.java
@@ -4,7 +4,7 @@
 //CONDIÇÃO: expressão booleana que testa a variável de controle de laço, determina se o laço irá interar ou encerrar.
 //INTERAÇÃO: determina como a variável do laço é alterada sempre que o laço intera.
 
-
+/* 
 public class javaFor {
     public static void main(String[] args) {
         int contagem;
@@ -14,4 +14,18 @@ public class javaFor {
 
     }
 
+}
+*/
+//agora um laço dentro do outro
+
+public class javaFor {
+
+    public static void main(String[] args) {
+        for(int i = 0; i <=10 ; i ++){
+            for(int j = 0; j <=10 ; j ++){
+                System.out.println(j * i);
+            }// o resultado foi ex: 10x1, 10x2,10x3,10x4,10x5,10x6,10x7,10x8,10x9,10x10,com todos os numeros de 0-10
+        }
+           
+    }
 }
